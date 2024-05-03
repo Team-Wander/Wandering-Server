fb0549658615ac36032191d7e16f21e09a8a96e1
taeyeon <tyeon42051@naver.com>
2024-05-03 11:57:14 +0900

    init

---
+++ src/test/java/com/wandering/Do/DoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wandering.Do;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
