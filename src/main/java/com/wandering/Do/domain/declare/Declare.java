1776cba744fa3f11b0afb19819ca539cc541d730
taeyeon <tyeon42051@naver.com>
2024-05-09 20:06:13 +0900

    add :: Declare

---
+++ src/main/java/com/wandering/Do/domain/declare/Declare.java
@@ -0,0 +1,34 @@
+package com.wandering.Do.domain.declare;
+
+import com.wandering.Do.domain.promise.entity.Promise;
+import com.wandering.Do.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Declare {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private Reason reason;
+
+    @ManyToOne
+    @JoinColumn(name = "promise_id")
+    private Promise promise;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
