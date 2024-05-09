89c9c1ec5dcb3ad3f2a441f6c25e8df7319319f4
taeyeon <tyeon42051@naver.com>
2024-05-09 20:06:36 +0900

    add :: Reason

---
+++ src/main/java/com/wandering/Do/domain/declare/Reason.java
@@ -0,0 +1,13 @@
+package com.wandering.Do.domain.declare;
+
+public enum Reason {
+    Personal_Info_Disclosure, // 개인 정보 노출
+
+    Same_Content, // 같은 내용 반복 작성
+    Abusive_Attacks, // 욕설, 인신 공격
+    Obscene_Sensationalism, // 음란, 선정성
+
+    Promotional_Commercial, // 홍보, 상업적
+
+    ETC // 기타
+}
