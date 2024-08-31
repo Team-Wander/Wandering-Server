18ee4c37b98e1eaef9390d7972853ff98693bf3d
yusung103 <gimyuseong742@gmail.com>
2024-08-31 23:11:44 +0900

    modified::Board->Promise

--- src/main/java/com/wandering/Do/domain/promise/exception/PromiseNotFoundException.java
+++ src/main/java/com/wandering/Do/domain/promise/exception/PromiseNotFoundException.java
@@ -3,8 +3,8 @@ package com.wandering.Do.domain.promise.exception;
 import com.wandering.Do.global.exception.CustomException;
 import com.wandering.Do.global.exception.ErrorCode;
 
-public class BoardNotFoundException extends CustomException {
-    public BoardNotFoundException() {
-        super(ErrorCode.BOARD_NOT_FOUND);
+public class PromiseNotFoundException extends CustomException {
+    public PromiseNotFoundException() {
+        super(ErrorCode.PROMISE_NOT_FOUND);
     }
 }
