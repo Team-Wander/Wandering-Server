9289827d2177adedbac803b24c70db909092f914
ta2ye0n <tyeon42051@naver.com>
2024-09-04 11:44:14 +0900

    update :: toEntity 적용

--- src/main/java/com/wandering/Do/domain/promise/service/impl/WriteBoardServiceImpl.java
+++ src/main/java/com/wandering/Do/domain/promise/service/impl/WriteBoardServiceImpl.java
@@ -1,17 +1,17 @@
 package com.wandering.Do.domain.promise.service.impl;
+
 import com.wandering.Do.domain.promise.entity.*;
-import com.wandering.Do.domain.promise.exception.InvalidDateException;
 import com.wandering.Do.domain.promise.exception.InvalidTagCountException;
 import com.wandering.Do.domain.promise.presentation.dto.req.PromiseWriteReq;
 import com.wandering.Do.domain.promise.repository.PromiseRepository;
 import com.wandering.Do.domain.promise.service.WriteBoardService;
+import com.wandering.Do.domain.promise.util.PromiseConverter;
 import com.wandering.Do.domain.user.entity.User;
 import com.wandering.Do.domain.user.util.UserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -21,6 +21,7 @@ public class WriteBoardServiceImpl implements WriteBoardService {
 
     private final PromiseRepository promiseRepository;
     private final UserUtil userUtil;
+    private final PromiseConverter promiseConverter;
 
 
     @Override
@@ -28,33 +29,13 @@ public class WriteBoardServiceImpl implements WriteBoardService {
 
         User user = userUtil.getCurrentUser();
 
-        LocalDate now = LocalDate.now();
-
         List<Tag> tags = writeReqDto.getTags();
         if (tags.size() > 2) {
             throw new InvalidTagCountException();
         }
 
-        Contact contact = new Contact(
-                writeReqDto.getContact().getInstagram(),
-                writeReqDto.getContact().getDiscord(),
-                writeReqDto.getContact().getKakao(),
-                writeReqDto.getContact().getEmail()
-        );
+        Promise promise = promiseConverter.toEntity(writeReqDto, user);
 
-        Promise promise = Promise.builder()
-                .title(writeReqDto.getTitle())
-                .content(writeReqDto.getContent())
-                .contact(contact)
-                .date(now)
-                .spot(writeReqDto.getSpot())
-                .maximum(writeReqDto.getMaximum())
-                .tags(tags)
-                .user(user)
-                .gender(SelGender.ANY)
-                .stats(Stats.PENDING)
-                .grade(writeReqDto.getGrade())
-                .build();
         promiseRepository.save(promise);
     }
 }
