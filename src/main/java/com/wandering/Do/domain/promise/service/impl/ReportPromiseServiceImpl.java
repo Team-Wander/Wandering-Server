2ccbb5ec07eee799e3de4739a4abfae7540b332e
ta2ye0n <tyeon42051@naver.com>
2024-09-07 20:15:11 +0900

    add :: 글 신고 기능

---
+++ src/main/java/com/wandering/Do/domain/promise/service/impl/ReportPromiseServiceImpl.java
@@ -0,0 +1,37 @@
+package com.wandering.Do.domain.promise.service.impl;
+
+import com.wandering.Do.domain.declare.entity.Report;
+import com.wandering.Do.domain.declare.repository.ReportRepository;
+import com.wandering.Do.domain.declare.util.ReportConverter;
+import com.wandering.Do.domain.promise.entity.Promise;
+import com.wandering.Do.domain.promise.exception.PromiseNotFoundException;
+import com.wandering.Do.domain.promise.presentation.dto.req.ReportPromiseReq;
+import com.wandering.Do.domain.promise.repository.PromiseRepository;
+import com.wandering.Do.domain.promise.service.ReportPromiseService;
+import com.wandering.Do.domain.user.entity.User;
+import com.wandering.Do.domain.user.util.UserUtil;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ReportPromiseServiceImpl implements ReportPromiseService {
+
+    private final PromiseRepository promiseRepository;
+    private final ReportRepository reportRepository;
+    private final UserUtil userUtil;
+    private final ReportConverter reportConverter;
+
+    public void execute(ReportPromiseReq reportPromiseReq, Long id) {
+        User user = userUtil.getCurrentUser();
+
+        Promise promise = promiseRepository.findById(id)
+                .orElseThrow(PromiseNotFoundException::new);
+
+        Report report = reportConverter.toEntity(reportPromiseReq, user, promise);
+
+        reportRepository.save(report);
+    }
+}
