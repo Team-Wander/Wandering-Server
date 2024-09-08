f465d0531c3423fa05df37f79d27238159d7b248
yusung103 <gimyuseong742@gmail.com>
2024-09-09 00:05:52 +0900

    modified::useJpa

--- src/main/java/com/wandering/Do/domain/promise/repository/PromiseRepository.java
+++ src/main/java/com/wandering/Do/domain/promise/repository/PromiseRepository.java
@@ -1,10 +1,15 @@
 package com.wandering.Do.domain.promise.repository;
 
 import com.wandering.Do.domain.promise.entity.Promise;
+import com.wandering.Do.domain.promise.entity.Tag;
+import com.wandering.Do.domain.user.entity.Gender;
+import com.wandering.Do.domain.user.entity.Grade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PromiseRepository extends JpaRepository<Promise, Long> {
     List<Promise> findBySpot(String spot);
+    List<Promise> findByTagsInAndGenderInAndGradeIn(
+            List<Tag> tag, List<Gender> gender, List<Grade> grade);
 }
