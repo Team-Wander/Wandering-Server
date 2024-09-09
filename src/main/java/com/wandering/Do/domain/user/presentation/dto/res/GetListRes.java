a12b384b1797b67a2743ad92eed12d9d63254b13
yusung103 <gimyuseong742@gmail.com>
2024-09-09 20:48:47 +0900

    add::userDto

---
+++ src/main/java/com/wandering/Do/domain/user/presentation/dto/res/GetListRes.java
@@ -0,0 +1,29 @@
+package com.wandering.Do.domain.user.presentation.dto.res;
+
+import com.wandering.Do.domain.promise.entity.Tag;
+import com.wandering.Do.domain.user.entity.Gender;
+import com.wandering.Do.domain.user.entity.Grade;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Builder
+public class GetListRes {
+    // user info
+    private String author;
+    private String author_school;
+    private Grade author_grade;
+    private String author_gender;
+
+    // promise
+    private Long id;
+    private String title;
+    private List<Grade> grade;
+    private Gender gender;
+    private LocalDate date;
+    private Integer maximum;
+    private List<Tag> tag;
+}
