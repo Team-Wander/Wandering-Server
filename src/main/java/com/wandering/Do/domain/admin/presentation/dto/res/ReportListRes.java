4056f6b307d2d4a100d9331898416f8fd760093e
ta2ye0n <tyeon42051@naver.com>
2024-09-09 16:51:11 +0900

    add :: ReportListRes

---
+++ src/main/java/com/wandering/Do/domain/admin/presentation/dto/res/ReportListRes.java
@@ -0,0 +1,28 @@
+package com.wandering.Do.domain.admin.presentation.dto.res;
+
+import com.wandering.Do.domain.declare.Reason;
+import com.wandering.Do.domain.promise.entity.Tag;
+import com.wandering.Do.domain.user.entity.Gender;
+import com.wandering.Do.domain.user.entity.Grade;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Builder
+public class ReportListRes {
+    private String author;
+    private String author_school;
+    private Grade author_grade;
+    private String author_gender;
+    private Reason reason;
+    private Long id;
+    private String title;
+    private List<Grade> grade;
+    private Gender gender;
+    private LocalDate date;
+    private Integer maximum;
+    private List<Tag> tag;
+}
