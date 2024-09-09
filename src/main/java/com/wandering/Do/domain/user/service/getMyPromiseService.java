5597869127d19120c71341e5d6680d3f975b1f9b
yusung103 <gimyuseong742@gmail.com>
2024-09-09 20:49:06 +0900

    add::userConverter, userDto

--- src/main/java/com/wandering/Do/domain/user/service/getMyPromiseService.java
+++ src/main/java/com/wandering/Do/domain/user/service/getMyPromiseService.java
@@ -3,9 +3,10 @@ package com.wandering.Do.domain.user.service;
 import com.wandering.Do.domain.promise.presentation.dto.res.PromiseGetListRes;
 import com.wandering.Do.domain.promise.presentation.dto.res.PromiseGetRes;
 import com.wandering.Do.domain.user.entity.User;
+import com.wandering.Do.domain.user.presentation.dto.res.GetListRes;
 
 import java.util.List;
 
 public interface getMyPromiseService {
-    List<PromiseGetListRes> execute();
+    List<GetListRes> execute();
 }
--- src/main/java/com/wandering/Do/domain/user/service/impl/getMyPromiseServiceImpl.java
+++ src/main/java/com/wandering/Do/domain/user/service/impl/getMyPromiseServiceImpl.java
@@ -5,8 +5,10 @@ import com.wandering.Do.domain.promise.presentation.dto.res.PromiseGetListRes;
 import com.wandering.Do.domain.promise.repository.PromiseRepository;
 import com.wandering.Do.domain.promise.util.PromiseConverter;
 import com.wandering.Do.domain.user.entity.User;
+import com.wandering.Do.domain.user.presentation.dto.res.GetListRes;
 import com.wandering.Do.domain.user.service.getMyPromiseService;
 import com.wandering.Do.domain.user.util.UserUtil;
+import com.wandering.Do.domain.user.util.converter.UserConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,14 +22,14 @@ import java.util.stream.Collectors;
 public class getMyPromiseServiceImpl implements getMyPromiseService {
     private final PromiseRepository promiseRepository;
     private final UserUtil userUtil;
-    private final PromiseConverter promiseConverter;
+    private final UserConverter userConverter;
     @Override
-    public List<PromiseGetListRes> execute() {
+    public List<GetListRes> execute() {
         User user = userUtil.getCurrentUser();
         List<Promise> promises = promiseRepository.findByUser(user);
 
         return promises.stream()
-                .map(promiseConverter::toListDto)
+                .map(userConverter::toListDto)
                 .collect(Collectors.toList());
     }
 }
