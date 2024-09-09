76acb2e23d93a4e975fefb025e72bd77df4f3427
ta2ye0n <tyeon42051@naver.com>
2024-09-09 11:07:04 +0900

    add :: endpoint 설정

---
+++ src/main/java/com/wandering/Do/domain/admin/presentation/AdminController.java
@@ -0,0 +1,26 @@
+package com.wandering.Do.domain.admin.presentation;
+
+import com.wandering.Do.domain.admin.presentation.dto.res.ReportListRes;
+import com.wandering.Do.domain.admin.service.GetReportListService;
+import com.wandering.Do.domain.declare.entity.Report;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final GetReportListService getReportListService;
+
+    @GetMapping("/dec_info")
+    public ResponseEntity<List<ReportListRes>> getReportList() {
+        List<ReportListRes> res = getReportListService.execute();
+        return ResponseEntity.ok(res);
+    }
+}
