9b0ffae197a866f9b444bfd006da72015c472dba
yusung103 <gimyuseong742@gmail.com>
2024-09-09 17:45:23 +0900

    modified::applyResponseEntity

--- src/main/java/com/wandering/Do/domain/user/presentation/UserController.java
+++ src/main/java/com/wandering/Do/domain/user/presentation/UserController.java
@@ -4,6 +4,7 @@ import com.wandering.Do.domain.promise.presentation.dto.res.PromiseGetListRes;
 import com.wandering.Do.domain.user.service.getMyPromiseService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,8 @@ import java.util.List;
 public class UserController {
     private final getMyPromiseService getMyPromiseService;
     @GetMapping
-    public List<PromiseGetListRes> getMy() {
-        return getMyPromiseService.execute();
+    public ResponseEntity<List<PromiseGetListRes>> getMy() {
+        List<PromiseGetListRes> res = getMyPromiseService.execute();
+        return ResponseEntity.ok(res);
     }
 }
