ed78c53728a8ecd78cab6024d3bdd46ba2ebf75c
skgur07 <pigjaoki0970@gmail.com>
2024-09-23 13:09:01 +0900

    update :: admin도 삭제되게 수정

--- src/main/java/com/wandering/Do/domain/promise/service/impl/DeletePromiseServiceImpl.java
+++ src/main/java/com/wandering/Do/domain/promise/service/impl/DeletePromiseServiceImpl.java
@@ -1,10 +1,13 @@
 package com.wandering.Do.domain.promise.service.impl;
 
+import com.wandering.Do.domain.admin.util.AdminConverter;
+import com.wandering.Do.domain.admin.util.impl.AdminConverterImpl;
 import com.wandering.Do.domain.promise.entity.Promise;
 import com.wandering.Do.domain.promise.exception.PromiseNotFoundException;
 import com.wandering.Do.domain.promise.exception.UserNotMatchException;
 import com.wandering.Do.domain.promise.repository.PromiseRepository;
 import com.wandering.Do.domain.promise.service.DeletePromiseService;
+import com.wandering.Do.domain.user.entity.Authority;
 import com.wandering.Do.domain.user.entity.User;
 import com.wandering.Do.domain.user.util.UserUtil;
 import com.wandering.Do.global.annotation.ServiceWithTransactional;
@@ -23,7 +26,7 @@ public class DeletePromiseServiceImpl implements DeletePromiseService {
         Promise promise = promiseRepository.findById(id)
                 .orElseThrow(PromiseNotFoundException::new);
 
-        if (user != promise.getUser())
+        if (user.getAuthority() != Authority.ADMIN && user != promise.getUser())
             throw new UserNotMatchException();
 
         promiseRepository.deleteById(id);
