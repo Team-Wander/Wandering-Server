a021de5c63f65102f97e0e80f51be0302f9ebc96
yusung103 <gimyuseong742@gmail.com>
2024-10-01 14:41:15 +0900

    add :: Exception

--- src/main/java/com/wandering/Do/domain/user/service/impl/GetWhoApplyServiceImpl.java
+++ src/main/java/com/wandering/Do/domain/user/service/impl/GetWhoApplyServiceImpl.java
@@ -3,9 +3,14 @@ package com.wandering.Do.domain.user.service.impl;
 import com.wandering.Do.domain.admin.exception.NotFoundReportException;
 import com.wandering.Do.domain.apply.entity.Application;
 import com.wandering.Do.domain.apply.repository.ApplicationRepository;
+import com.wandering.Do.domain.promise.entity.Promise;
 import com.wandering.Do.domain.promise.exception.PromiseNotFoundException;
+import com.wandering.Do.domain.promise.exception.UserNotMatchException;
+import com.wandering.Do.domain.promise.repository.PromiseRepository;
+import com.wandering.Do.domain.user.entity.User;
 import com.wandering.Do.domain.user.presentation.dto.res.GetWhoApplyPromiseRes;
 import com.wandering.Do.domain.user.service.GetWhoApplyService;
+import com.wandering.Do.domain.user.util.UserUtil;
 import com.wandering.Do.domain.user.util.converter.UserConverter;
 import com.wandering.Do.global.annotation.ServiceWithTransactionalReadOnly;
 import lombok.RequiredArgsConstructor;
@@ -17,13 +22,19 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class GetWhoApplyServiceImpl implements GetWhoApplyService {
     private final ApplicationRepository applicationRepository;
+    private final PromiseRepository promiseRepository;
     private final UserConverter userConverter;
-
+    private final UserUtil userUtil;
     public List<GetWhoApplyPromiseRes> execute(Long promiseId) {
-        List<Application> applicationList = applicationRepository.findByPromiseId(promiseId);
+        User user = userUtil.getCurrentUser();
 
-        if(applicationList.isEmpty())
-            throw new PromiseNotFoundException();
+        Promise promise = promiseRepository.findById(promiseId)
+                .orElseThrow(NotFoundReportException::new);
+
+        if(!promise.getUser().equals(user))
+            throw new UserNotMatchException();
+
+        List<Application> applicationList = applicationRepository.findByPromiseId(promiseId);
 
         return applicationList.stream()
                 .map(application -> userConverter.toDto(application.getUser()))
