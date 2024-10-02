6f4fa308ea92560de23960e737e51faa3f830566
ta2ye0n <tyeon42051@naver.com>
2024-10-02 11:17:21 +0900

    add :: toUserDto

--- src/main/java/com/wandering/Do/domain/admin/util/impl/AdminConverterImpl.java
+++ src/main/java/com/wandering/Do/domain/admin/util/impl/AdminConverterImpl.java
@@ -2,9 +2,11 @@ package com.wandering.Do.domain.admin.util.impl;
 
 import com.wandering.Do.domain.admin.presentation.dto.res.ReportInfoRes;
 import com.wandering.Do.domain.admin.presentation.dto.res.ReportListRes;
+import com.wandering.Do.domain.admin.presentation.dto.res.UserListRes;
 import com.wandering.Do.domain.admin.util.AdminConverter;
 import com.wandering.Do.domain.declare.entity.Report;
 import com.wandering.Do.domain.promise.entity.Promise;
+import com.wandering.Do.domain.user.entity.User;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -40,6 +42,17 @@ public class AdminConverterImpl implements AdminConverter {
                 .tag(promise.getTags())
                 .contact(promise.getContact())
                 .spot(promise.getSpot())
+                .build();
+    }
+
+    public UserListRes toUserDto(User user) {
+        return UserListRes.builder()
+                .userId(user.getId())
+                .name(user.getName())
+                .school(user.getSchool())
+                .gender(user.getGender())
+                .profile(user.getImage())
+                .aCase(user.getACase())
                 .build();
     }
 }
