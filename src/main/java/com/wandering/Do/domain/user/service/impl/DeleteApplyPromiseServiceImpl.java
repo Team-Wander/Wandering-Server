4d3608f2f0880e9b1dca71e784f3d61288e9e178
yusung103 <gimyuseong742@gmail.com>
2024-10-10 12:22:48 +0900

    modify :: DeleteApplyPromise

--- src/main/java/com/wandering/Do/domain/user/service/impl/DeleteApplyPromiseServiceImpl.java
+++ src/main/java/com/wandering/Do/domain/user/service/impl/DeleteApplyPromiseServiceImpl.java
@@ -3,8 +3,8 @@ package com.wandering.Do.domain.user.service.impl;
 import com.wandering.Do.domain.apply.entity.Application;
 import com.wandering.Do.domain.apply.repository.ApplicationRepository;
 import com.wandering.Do.domain.promise.exception.PromiseNotFoundException;
-import com.wandering.Do.domain.promise.exception.UserNotMatchException;
 import com.wandering.Do.domain.user.entity.User;
+import com.wandering.Do.domain.user.exception.NotIncludedApplicationException;
 import com.wandering.Do.domain.user.service.DeleteApplyPromiseService;
 import com.wandering.Do.domain.user.util.UserUtil;
 import com.wandering.Do.global.annotation.ServiceWithTransactional;
@@ -15,14 +15,14 @@ import lombok.RequiredArgsConstructor;
 public class DeleteApplyPromiseServiceImpl implements DeleteApplyPromiseService {
     private final ApplicationRepository applicationRepository;
     private final UserUtil userUtil;
-    public void execute(Long id) {
+    public void execute(Long promiseId) {
         User user = userUtil.getCurrentUser();
 
-        Application application = applicationRepository.findById(id)
+        Application application = applicationRepository.findByPromiseIdAndUserId(promiseId, user.getId())
                 .orElseThrow(PromiseNotFoundException::new);
 
         if(!application.getUser().getId().equals(user.getId()))
-            throw new UserNotMatchException();
+            throw new NotIncludedApplicationException();
 
         applicationRepository.delete(application);
     }
