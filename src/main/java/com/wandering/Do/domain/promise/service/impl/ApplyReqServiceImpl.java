af6883d6178687833b5610cc0728359466374099
ta2ye0n <tyeon42051@naver.com>
2024-10-16 21:03:06 +0900

    add :: count

--- src/main/java/com/wandering/Do/domain/promise/service/impl/ApplyReqServiceImpl.java
+++ src/main/java/com/wandering/Do/domain/promise/service/impl/ApplyReqServiceImpl.java
@@ -4,6 +4,7 @@ import com.wandering.Do.domain.apply.entity.Application;
 import com.wandering.Do.domain.promise.entity.Promise;
 import com.wandering.Do.domain.promise.exception.AlreadyAppliedException;
 import com.wandering.Do.domain.promise.exception.IllegalArgumentException;
+import com.wandering.Do.domain.promise.exception.LimitExceededException;
 import com.wandering.Do.domain.promise.exception.PromiseNotFoundException;
 import com.wandering.Do.domain.apply.repository.ApplicationRepository;
 import com.wandering.Do.domain.promise.repository.PromiseRepository;
@@ -27,13 +28,22 @@ public class ApplyReqServiceImpl implements ApplyReqService {
         Promise promise = promiseRepository.findById(id)
                 .orElseThrow(PromiseNotFoundException::new);
 
-        if(promise.getUser().getId().equals(user.getId()))
-            throw new IllegalArgumentException();
+//        if(promise.getUser().getId().equals(user.getId()))
+//            throw new IllegalArgumentException();
 
-        boolean alreadyApplied = applicationRepository.existsByPromiseAndUser(promise, user);
-        if (alreadyApplied) {
-            throw new AlreadyAppliedException();
-        }
+//        boolean alreadyApplied = applicationRepository.existsByPromiseAndUser(promise, user);
+//        if (alreadyApplied) {
+//            throw new AlreadyAppliedException();
+//        }
+
+        if (promise.getCount() == promise.getMaximum())
+            throw new LimitExceededException();
+        else promise.increaseCount();
+//        if (applicationRepository.existsByPromiseAndUser(promise, user)) {
+//            throw new AlreadyAppliedException();
+//        } else if (promise.getCount() == promise.getMaximum()) {
+//            throw new LimitExceededException();
+//        } else promise.increaseCount();
 
         Application application = new Application(promise, user);
 
