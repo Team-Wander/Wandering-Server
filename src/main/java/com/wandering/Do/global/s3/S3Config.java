26c4bc65f729c0ae990dad40b73523834a130316
yusung103 <gimyuseong742@gmail.com>
2024-11-06 16:00:16 +0900

    modify :: Configuration

--- src/main/java/com/wandering/Do/global/s3/S3Config.java
+++ src/main/java/com/wandering/Do/global/s3/S3Config.java
@@ -5,11 +5,11 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Configurable
+@Configuration
 public class S3Config {
 
     @Value("${cloud.aws.credentials.access-key}")
